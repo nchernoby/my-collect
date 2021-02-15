fc7396050b569d5804df64ef73c59b77dba80ab4
Nicole Chernoby <n.chernoby@gmail.com>
2021-02-14 18:37:46 -0800

    commit tree 071e4670848628598077c0c56113784afe9cba97

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
+def my_collect(array)
+   i = 0
+   collection = []
+   while i < array.length
+     collection << yield(array[i])
+     i += 1
+   end
+   collection.upcase
+end
 
-
+def my_collect(students)
+   i = 0
+   while i < students.size
+     yield(students[i])
+     i += 1
+   end
+   students.map{|i| i.split(" ").first}
+end
